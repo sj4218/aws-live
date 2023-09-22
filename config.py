679baec8cb4048c9a47519b10da126348f41c85c
sj4218 <79525917+sj4218@users.noreply.github.com>
2023-09-22 22:40:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cawawmgwtlq5.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanszejie-employee"
+customregion = "us-east-1a"
+
